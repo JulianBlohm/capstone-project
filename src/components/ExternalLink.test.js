4184b4ad34e2f2961d29ec856ed56d3f63d89882
Julian Blohm <julianscript@outlook.de>
2020-12-27 13:34:25 +0100

    Fix unit tests

--- src/components/ExternalLink.test.js
+++ src/components/ExternalLink.test.js
@@ -8,4 +8,15 @@ describe('ExternalLink', () => {
         )
         expect(getByText('Bundesregierung')).toBeInTheDocument()
     })
+
+    it('has an href and a target', () => {
+        const { getByRole } = render(
+            <ExternalLink href="www.test.de" target="_blank">
+                Bundesregierung
+            </ExternalLink>
+        )
+        const link = getByRole('link')
+        expect(link).toHaveAttribute('href', 'www.test.de')
+        expect(link).toHaveAttribute('target', '_blank')
+    })
 })
--- src/components/FAQ.test.js
+++ src/components/FAQ.test.js
@@ -51,4 +51,37 @@ describe('FAQ', () => {
         const answerText = queryByText('Alles wird benutzt')
         expect(answerText).not.toBeInTheDocument()
     })
+
+    it('renders correctly with several questions', () => {
+        const dataMock = [
+            {
+                id: 1,
+                question: 'Was wird benutzt?',
+                answer: 'Alles wird benutzt',
+            },
+            {
+                id: 2,
+                question: 'Was ist passiert?',
+                answer: 'Nichts ist passiert',
+            },
+            {
+                id: 3,
+                question: 'Was ist los?',
+                answer: 'Viel',
+            },
+        ]
+        const { getByText, queryByText } = render(<FAQ data={dataMock} />)
+
+        expect(getByText('Was wird benutzt?')).toBeInTheDocument()
+        expect(getByText('Was ist passiert?')).toBeInTheDocument()
+        expect(getByText('Was ist los?')).toBeInTheDocument()
+
+        const answerText1 = queryByText('Alles wird benutzt')
+        const answerText2 = queryByText('Nichts ist passiert')
+        const answerText3 = queryByText('Viel')
+
+        expect(answerText1).not.toBeInTheDocument()
+        expect(answerText2).not.toBeInTheDocument()
+        expect(answerText3).not.toBeInTheDocument()
+    })
 })
